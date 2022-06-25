a58dab5b0241e6afff11a32280d8c1c2c8780fa0
andrei.oancea <andrei.oancea@192-168-0-167.rdsnet.ro>
2022-06-25 17:11:33 +0300

    Extended calculator functionality

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ const myButton = document.getElementById('myButton');
 function calculate() {
     let dream = parseInt(document.getElementById('dream').value);
     let money = parseInt(document.getElementById('money').value);
-    let resultM = document.getElementById('resultM').value = dream / money;
-    let resultY = document.getElementById('resultY').value = ((dream / money) / 12).toFixed(1);
+    let select = document.getElementById('period');
+    let value = select.options[select.selectedIndex].value;
+
+    if (value === 'd') {
+        document.getElementById('resultM').value = parseFloat(((dream / money) / 30).toFixed(1));
+        document.getElementById('resultY').value = parseFloat(((dream / money) / 365).toFixed(1));
+    } else if (value === 'm') {
+        document.getElementById('resultM').value = parseFloat((dream / money).toFixed(1));
+        document.getElementById('resultY').value = parseFloat(((dream / money) / 12).toFixed(1));
+    } else if (value === 'y') {
+        document.getElementById('resultM').value = parseFloat(((dream / money) * 12).toFixed(1));
+        document.getElementById('resultY').value = parseFloat((dream / money).toFixed(1));
+    }
+
+    // let resultM = document.getElementById('resultM').value = dream / money;
+    // let resultY = document.getElementById('resultY').value = ((dream / money) / 12).toFixed(1);
 }
 
 myButton.addEventListener('click', calculate);
